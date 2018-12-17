3b34a24c34efc5ede42d1242e3adb096b669d4dd
Lily <lilydanhaive@gmail.com>
2018-12-17 15:09:08 +0100

    proces env

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
-app = express();
+const app = express();
 
 
 app.use(express.static('public'));
 app.use('/', express.static(__dirname + '/public/Homepage.html'));
 
-const port =3000 || process.env.PORT;
+const port = process.env.PORT;
 
 app.listen( port, () => {
     console.log('Server is running')
