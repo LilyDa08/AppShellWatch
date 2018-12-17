0f99220a01f3c2f8b08e0a033cc7f33b22aa0f5c
Lily <lilydanhaive@gmail.com>
2018-12-17 15:01:54 +0100

    port

--- server.js
+++ server.js
@@ -5,3 +5,9 @@ app = express();
 
 app.use(express.static('public'));
 app.use('/', express.static(__dirname + '/public/Homepage.html'));
+
+const port = 80 || process.env.PORT;
+
+app.listen( port, () => {
+    console.log('Server is running')
+});
