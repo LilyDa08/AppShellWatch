f92f42a6f928284678bbc8a1e46304c68abd101d
Lily <lilydanhaive@gmail.com>
2018-12-18 09:37:39 +0100

    backto one

--- public/sw.js
+++ public/sw.js
@@ -1,79 +1,30 @@
-var cacheName = 'shell-content.V.19';
-var filesToCache = [
-  './index.html',
-  './assets/css/offline.css'
-];
 
-self.addEventListener('install', function (e) {
-  console.log('[ServiceWorker] Install');
+let version = "1";
+const cacheName = `FD-${version}`;
+
+self.addEventListener('install', e => {
   e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/assets/css/offline.css`
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
     caches.open(cacheName)
-    .then(function (cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
     })
   );
 });
-
-
-self.addEventListener('activate', function (e) {
-  console.log('[Service Worker] activate');
-
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      return Promise.all(keyList.map(function (key) {
-        if (key !== cacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-});
-
-
-self.addEventListener('fetch', function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
-    })
-  )
-})
-
-
-// self.addEventListener('fetch', function (event) {
-//   console.log('Fetch event for ', event.request.url);
-//   event.respondWith(
-//     caches.match(event.request).then(function (response) {
-//       if (response) {
-//         console.log('Found ', event.request.url, ' in cache');
-//         return response;
-//       } 
-//       console.log('Network request for ', event.request.url);
-//       return fetch(event.request)
-
-//     }).catch(function(err) {
-//   console.log('failed ', err);
-// });
-//   );
-// });
-
-
-
-// self.addEventListener('fetch', function(event) {
-//   console.log('Fetch event for ', event.request.url);
-//   event.respondWith(
-//     caches.match(event.request).then(function(response) {
-//       if (response) {
-//         console.log('Found ', event.request.url, ' in cache');
-//         return response;
-//       }
-//       console.log('Network request for ', event.request.url);
-//       return fetch(event.request)
-
-//     }).catch(function(error) {
-
-//       return caches.match('index.html');
-
-//     })
-//   );
